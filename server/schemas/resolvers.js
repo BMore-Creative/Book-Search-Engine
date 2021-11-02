1bedad5a115a73599ba961c5cc080aee40e9f877
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-11-02 14:37:24 -0700

    added resolvers for queries and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,15 +3,75 @@ const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userInfo =  User.findOne({ _id: context.user._id }).populate('savedBooks');
-            
-                return userInfo;
-            }
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userInfo = User.findOne({ _id: context.user._id }).populate(
+          "savedBooks"
+        );
 
-            throw new AuthenticationError('No user with this id found!')
-        }
-    }
-}
+        return userInfo;
+      }
+
+      throw new AuthenticationError("No user with this id found!");
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Entered Email/Password not found");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({
+        username,
+        email,
+        password,
+      });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdandUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: input } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("You aren't logged in!");
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdandUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: bookId } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("You aren't logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
