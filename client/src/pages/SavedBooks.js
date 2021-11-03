6d0448d046f1f3352d145a7f4da0fec23f7fcf6c
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-11-02 20:09:37 -0700

    misc typo fixes/formatting changes to ensure proper server functionality

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Jumbotron,
   Container,
@@ -40,7 +40,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
